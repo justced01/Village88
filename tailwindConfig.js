09179c31d2b01f5fdb4751d31b2bd28578001b83
Newbie <jctdelacarcel@gmail.com>
2022-03-05 23:02:47 +0800

    added tailwind css

---
+++ tailwindConfig.js
@@ -0,0 +1,16 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      screens: {
+        'tablet': {'max': '1024px'},
+        'mobile': {'max': '480px'}
+      },
+      colors: {
+        'silver': '#C0C0C0'
+      },
+      fontFamily: {
+          Roboto: ['Roboto']
+      }
+    }
+  }
+}
